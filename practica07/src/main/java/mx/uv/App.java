a1ac232d5d5c059c86b791cbc679a6f9f67e634b
MycW3 <mari.carmenvidal266@gmail.com>
2022-11-09 14:08:01 -0600

    practica07

---
+++ practica07/src/main/java/mx/uv/App.java
@@ -0,0 +1,34 @@
+package mx.uv;
+
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+public class App
+{
+    public static Gson gson = new Gson();
+    public static Map<String, Usuario> usuarios = new HashMap<>();
+    public static void main( String[] args )
+    {
+        
+        //inicialización de datos
+        Usuario u1 = new Usuario("1", "pablo", "123");
+        Usuario u2 = new Usuario("2", "Ana", "12345");
+        usuarios.put(u1.getId(), u1);
+        usuarios.put(u2.getId(), u2);
+
+        System.out.println( "Hello World!" );
+        before((req, res) -> res.type("application/json"));
+        get( "/usuario", (req, res)-> gson.toJson(u1));
+        get( "/usuarios", (req, res)-> gson.toJson(usuarios));
+
+        post("/usuarios", (req, res)->{
+            String datosFormulario = req.body();
+            Usuario u = gson.fromJson(datosFormulario, Usuario.class);
+            usuarios.put(u.getId(), u);
+            return "usuario agregado";
+        });
+    }
+}
